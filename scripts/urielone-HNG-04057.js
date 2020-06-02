f8f749b8f867d4ddaaf37e14b5b15a9c942c2309
x3meuriel <x3me7@hotmail.com>
2020-06-02 23:41:52 +0100

    urielone-HNG-04057 script on team fierce

---
+++ scripts/urielone-HNG-04057.js
@@ -0,0 +1,11 @@
+
+let tf_urielone = {
+    name: "Ukwueji Okechukwu Emmanuel",
+    id: "HNG-04057",
+    email: "x3me7@hotmail.com",
+    language: "javascript"
+}
+
+let urielone_msg = "Hello World, My name is " + tf_urielone.name + " with HNGi7 id of " + tf_urielone.id + " and email: " + tf_urielone.email + ", i used " + tf_urielone.language + " scripting language for the task 2";
+
+console.log(urielone_msg);

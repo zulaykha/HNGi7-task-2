2ac6ddfd8081b90bfbb9a12016fcc175fbf80fd6
james afolaranmi <olaoluwaafolaranmi@gmail.com>
2020-06-03 10:43:08 +0100

    olajames-HNG-03683 script

---
+++ scripts/olajames-HNG-03683.py
@@ -0,0 +1,6 @@
+hngid = '03683'
+name = " Afolaranmi James Olaoluwa "
+def team_task():
+    print(f"""Hello world, this is {name} with HNGi7 ID {hngid} using python for stage 2 task""")
+
+team_task()

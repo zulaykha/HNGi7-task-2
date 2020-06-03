0d6613cc07662d3c478a597aa1c65141c39e5962
odomfavour <ogochukwuodom@gmail.com>
2020-06-03 12:08:08 +0100

    Ogochuks-HNG-01707 script

--- scripts/Ogochuks-HNG-01707.js
+++ scripts/Ogochuks-HNG-01707.js
@@ -3,9 +3,9 @@ const id = "HNG-01707";
 const language = "JavaScript";
 const email = "ogochukwuodom@gmail.com"
 
-printString(fullName, id, language, email)
+printDetails(fullName, id, language, email)
 
-function printString(name, id, lang, email) {
+function printDetails(name, id, lang, email) {
     console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`);
 
     return 

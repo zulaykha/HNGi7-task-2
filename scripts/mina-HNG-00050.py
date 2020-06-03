f6821b00c21fb68f42810349f071558fb9a730bc
Amina <63821349+Amina-cs50@users.noreply.github.com>
2020-06-03 11:35:39 +0100

    mina-HNG-00050.py
    Added email

--- scripts/mina-HNG-00050.py
+++ scripts/mina-HNG-00050.py
@@ -3,4 +3,4 @@ Email = "shitta19@gmail.com"
 Id = "HNG-00050"
 Language = "python"
 
-print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task" %(Name, Id, Language))
+print("Hello World, this is %s with HNGi7 ID %s and email %s using %s for stage 2 task" %(Name, Id, Email, Language))

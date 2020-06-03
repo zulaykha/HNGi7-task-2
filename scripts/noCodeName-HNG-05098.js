9110396314a0d17c7a69b4bff498d1fb0dd319af
kenrelic <simeonkenneth3@gmail.com>
2020-06-03 12:19:31 +0100

    noCodeName-HNG-05098 script

---
+++ scripts/noCodeName-HNG-05098.js
@@ -0,0 +1,7 @@
+//pass arguments and call.
+printTask2Info("Simeon Kenneth", "HNG-05098","JavaScript");
+
+//script to display a brief info about me, the task completed and the language used
+function printTask2Info(fullName,ID,language){
+    return console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task.`);
+} 

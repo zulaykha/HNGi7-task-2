47a56da0ec8ff3397d07394afa5b546582630b91
adessica <66354001+adessica@users.noreply.github.com>
2020-06-03 22:13:45 +0100

    adessica-HNG-06241 script

---
+++ scripts/adessica-HNG-06241.js
@@ -0,0 +1,11 @@
+// To start I'll create my details as variables
+var myFullname = 'Jessica Adaku Ugwu';
+var myID = 'HNG-06241';
+var myLanguage = 'JavaScript';
+var myMail = 'adaugwu10@gmail.com'; 
+
+// Create sentence by inserting variables in place
+var myScript = "Hello World, this is "+ myFullname + " with HNGi7 ID " + myID + ' and e-mail ' + myMail + " using "  + myLanguage +" for Stage Task 2.";
+
+//Print the final result
+document.write(myScript);

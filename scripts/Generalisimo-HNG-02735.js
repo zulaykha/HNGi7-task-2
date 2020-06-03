0db0da2f488f44510dac3dacebce7104e595f4d4
Emmanuel Ugwu <32464178+generalisimo777@users.noreply.github.com>
2020-06-03 13:09:46 +0100

    Updated Generalisimo-HNG-02735.js

--- scripts/Generalisimo-HNG-02735.js
+++ scripts/Generalisimo-HNG-02735.js
@@ -1,7 +1,8 @@
 var genId = "HNG-02735";
 var genName = "Emmanuel Ugwu";
 var genLang = "JavaScript";
+var genEmail = "ugwuec14@gmail.com"
 
-var fullId = "Hello World, this is" + " " + genName + " " + "with HNGi7 ID" + genId + " " + "using" + genLang + " " + "for stage 2 task."
+var fullId = "Hello World, this is" + " " + genName + " " + "with HNGi7 ID" + genId + "and email" + " " + genEmail + " " + "using" + genLang + " " + "for stage 2 task."
 
 console.log(fullId);

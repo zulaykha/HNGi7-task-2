8ffd4330b50657daa19b87574075d2fe22033862
Oluwaseun Sofoluwe <sofoluweseun@gmail.com>
2020-06-03 10:23:49 +0100

    d18thday-HNG-00677.js

---
+++ scripts/d18thday-HNG-00677.js
@@ -0,0 +1,10 @@
+
+let fullName = "Oluwaseun Sofoluwe";
+let HNGID = "HNG-00677";
+let language = "Javascript"
+
+let displayMessage = function(name, ID, language){
+    console.log(`Hello World, this is ${name} with HNGi7 ${ID} using ${language} for stage 2 task`);
+}
+
+displayMessage(fullName, HNGID, language);

5c23df1dd427b62690df5dff6379a63f2626a09e
AbdulHameed <30466736+Ahmed021@users.noreply.github.com>
2020-06-03 12:23:05 +0100

    Add files via upload
    abdulhameed-HNG-02134 script

---
+++ scripts/abdulhameed-HNG-02134.js
@@ -0,0 +1,5 @@
+var name = "AbdulHameed Onibiyo";
+var email = "ahmedabiodun4@gmail.com";
+var id = "HNG-02134";
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using Javascript for stage 2 task.");

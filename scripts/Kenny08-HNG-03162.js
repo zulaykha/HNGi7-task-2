ab3a341a4090351d47ada214829e3a38013af83c
kennyOlakunle <olakunlemuyiwa@gmail.com>
2020-06-03 11:00:31 +0100

    Kenny08-HNG-03162 script

---
+++ scripts/Kenny08-HNG-03162.js
@@ -0,0 +1,6 @@
+function HNG() {
+  return console.log(
+    `Hello World, this is Kehinde Abe with HNGi7 ID HNG-03162 and email olakunlemuyiwa@gmail.com using JavaScript for stage 2 task`
+  );
+}
+HNG();

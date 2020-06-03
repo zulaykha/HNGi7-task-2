d02759fd91b20d8a20ed6c07f2f9f0a368e7a35a
ikennaoyiih@gmail.com <61995586+I-GiM@users.noreply.github.com>
2020-06-03 13:12:24 +0100

    GiM-HNG-00165 script

---
+++ scripts/GiM-HNG-00165.js
@@ -0,0 +1,12 @@
+//Task 2 for HNGi7
+
+//HNGi7 Info
+const name = 'Ikenna Oyiih'
+const email = 'ikennaoyiih@gmail.com'
+const id = 'HNG-00165'
+const language = 'JavaScript'
+
+//Text to output
+let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.`
+
+return console.log(output);

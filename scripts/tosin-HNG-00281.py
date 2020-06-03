bb60aa961612a11552bbf70d77d09f2a07e6a27b
Bamidele OLUWATOSIN <65241457+tomisi@users.noreply.github.com>
2020-06-03 17:01:19 +0100

    tosin-HNG-00281.py

---
+++ scripts/tosin-HNG-00281.py
@@ -0,0 +1,5 @@
+name = "Bamidele oluwatosin"
+id = "00281"
+language = "python"
+email_address = "tomisinkemi468@gmail.com"
+output = f"My name is {name} with HNGi7 ID {id} using {language} for stage 2 task {email_address}"

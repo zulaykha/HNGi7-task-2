96910f9824bc2245ce8164b0ebf9f0e716e82890
AyoEmmanuel <nuelayo97@gmail.com>
2020-06-03 16:40:10 +0100

    Ay-HNG-00085 script

---
+++ scripts/Ay-HNG-00085.js
@@ -0,0 +1,14 @@
+
+
+
+
+// Script for Emmanuel Ayo's Info
+
+let myInternInfo = (fullName, hngId, email, language) => {
+
+    let myOutputMsg = `Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`;
+
+    return myOutputMsg;
+}
+console.log(myInternInfo("Emmanuel Ayo", "HNG-00085", 
+                        "nuelayo97@gmail.com", "Javascript"));

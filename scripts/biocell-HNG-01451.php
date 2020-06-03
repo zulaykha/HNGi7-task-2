316aa6c548c4acb124a08a5e66deb3137290112f
biocell <biocell@users.noreply.github.com>
2020-06-02 23:02:05 -1100

    Create biocell-HNG-01451.php

---
+++ scripts/biocell-HNG-01451.php
@@ -0,0 +1,10 @@
+<?php
+
+$first_name = "Abiodun";
+$last_name = "Abolarinwa";
+$hng_id= "HNG-01451";
+$full_name = $first_name. " " . $last_name;
+$message = "Hello World, this is ". $full_name . " with HNGi7 ID ". $hng_id . " using php for stage 2 task.";
+
+echo($message);
+?>

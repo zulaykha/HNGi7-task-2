dc6d3e0633206ac2a6876029dcd8b0470bfe2b94
calebjesusegun <jesuseguncaleb@gmail.com>
2020-06-03 18:41:18 +0100

    caleb.jesusegun-HNG-04913 JS Script

--- scripts/caleb.jesusegun-HNG-04913.js
+++ scripts/caleb.jesusegun-HNG-04913.js
@@ -3,11 +3,11 @@
 
 let firstName = "Caleb";
 let lastName = "Jesusegun";
-let hng_ID = "HNG-04913";
+let hngID = "HNG-04913";
 let email = "adeiyejesu@gmail.com";
 
 let stage2Task = () => {
-   return console.log(`Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hng_ID} and email ${email} using JavaScript for Stage 2 task.`)
+   return console.log(`Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} and email ${email} using JavaScript for Stage 2 task.`);
 };
 
 stage2Task();

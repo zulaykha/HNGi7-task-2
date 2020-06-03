ae0ffd1964065d8f54905a470c711c299921341b
Orluchy <favourboi86@gmail.com>
2020-06-03 16:30:19 +0100

    orluchy-HNG-01829 script

---
+++ scripts/Orluchy-HNG-01829.js
@@ -0,0 +1,6 @@
+let myname = "Favour Oluchi Boi";
+let myid = "HNG-01829";
+let mymail = "favourboi86@gmail.com";
+let preflang = "JavaScript";
+
+console.log(`Hello World, this is ${myname} with HNGi7 ID ${myid} and email ${mymail} using ${preflang} for stage 2 task`);

5904d4839eaaf93a61a6473897087190da928ce0
fakoredeDamilola <47531232+fakoredeDamilola@users.noreply.github.com>
2020-06-03 11:28:25 +0100

    Update fakorede.damilola-HNG-02102.js

--- scripts/fakorede.damilola-HNG-02102.js
+++ scripts/fakorede.damilola-HNG-02102.js
@@ -1,5 +1,5 @@
 function printInfo() {
-  return "Hello World, this is Fakorede Damilola with HNGi7 ID HNG-02102 using JavaScript for stage 2 task";
+  return "Hello World, this is Fakorede Damilola with HNGi7 ID HNG-02102 and email dfakorede29@gmail.com using JavaScript for stage 2 task";
 }
 
 console.log(printInfo());

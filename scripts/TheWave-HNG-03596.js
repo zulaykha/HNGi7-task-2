c93c8ee4ee6178f93b9bf21888b1b2c0d8008599
Egbedokun Olayinka <egbedokunolayinka@gmail.com>
2020-06-03 19:50:16 +0100

    Script by Egbedokun Olayinka with ID HNG-03596

---
+++ scripts/TheWave-HNG-03596.js
@@ -0,0 +1,15 @@
+const name = 'Egbedokun Olayinka';
+const id = 'HNG-03596';
+const language = 'Javascript';
+const email = 'egbedokunolayinka@gmail.com';
+
+const output = function(num) {
+    if(num % 2 === 0) {
+        return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+    } else {
+        return null;
+    }  
+}
+
+console.log(output(66));
+

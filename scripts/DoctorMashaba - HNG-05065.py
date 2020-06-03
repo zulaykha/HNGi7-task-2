29d3ce68a0534b6b4bcddce15b2ecc7ff8d6b936
Doctor Mashaba <37573070+DoctorMashaba@users.noreply.github.com>
2020-06-03 15:29:28 +0200

    Add files via upload

---
+++ scripts/DoctorMashaba - HNG-05065.py
@@ -0,0 +1,5 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello World, this Doctor Mashaba with HNGi7 ID HNG-05065 using Python for stage 2 task")
+Hello World, this Doctor Mashaba with HNGi7 ID HNG-05065 using Python for stage 2 task
+>>> 

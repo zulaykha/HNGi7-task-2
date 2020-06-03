9096c54ac943b7fd31c3c3a930fc01b642a0f3b2
CHIAZAM-TECH <66219168+CHIAZAM-TECH@users.noreply.github.com>
2020-06-03 11:38:36 +0100

    Printing Intern's  HNG-02112 information
    This is a JavaScript that prints out an Intern's  Information

---
+++ scripts/Chiazam-HNG-02112.js
@@ -0,0 +1,7 @@
+const internName = 'Ogbonnaya Chinonyerem Chiazam'; //this variable holds thhe variable for the intern name
+const internID = 'HND-02112'; //this variable holds the intern's ID
+const programLangauge = 'javascript'; //this variable holds the programl Langauge being used by the intern
+const internEmail = 'ochiazam@gmail.com'; // the interns e-mail address
+
+//this statement prints the information the console
+console.log(' Hello Word, this is ' + internName + ' with HNGi7 ID ' + internID + ' and email ' + internEmail + ' using ' + programLangauge + ' for stage 2 task ');

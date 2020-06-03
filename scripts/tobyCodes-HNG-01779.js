d9d8837d035d56024f0ea748b920b4c988ed1ca8
TobyCodes <twiseas9ice@gmail.com>
2020-06-03 20:23:22 +0100

    tobyCodes-HNG-01779 script

--- scripts/tobyCodes-HNG-01779.js
+++ scripts/tobyCodes-HNG-01779.js
@@ -1,9 +1,10 @@
+//DECLARE VARIABLES 
 const name = 'Paul Tobiloba';
 const id = 'HNG-01779';
 const email = 'twiseas9ice@gmail.com';
 const language = 'Javascript';
 
-
+//OUTPUT
 const output = () => {
     return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
 }

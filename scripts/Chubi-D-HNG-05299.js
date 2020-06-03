82d5c547ef0123ab10b4ed0d157dcb7eb1e9a85a
Chubi-D <chukwubuikempd@gmail.com>
2020-06-03 20:28:11 +0100

    Chubi-D-HNG-05299 script

---
+++ scripts/Chubi-D-HNG-05299.js
@@ -0,0 +1,14 @@
+/* Participant Information*/
+
+let fullName = "Okoro Chukwubuikem";
+let email= "chukwubuikempd@gmail.com";
+let hngi7ID= "HNG-05299";
+let language= "Javascript";
+
+/*Introductory Section*/
+
+let introduction = "Hello World, this is " +fullName+" with HNGi7 ID " +hngi7ID+" using "+language+" for stage 2 task."
+
+/* Printing the information out*/
+
+console.log(introduction);

02d7811dc52ae71b4b1191a2a0972f5c3bb34748
omodauda <omodauda.dl@gmail.com>
2020-06-03 23:15:03 +0100

    omodauda-HNG-00717 script

--- scripts/omodauda-HNG-00717.js
+++ scripts/omodauda-HNG-00717.js
@@ -1,8 +1,8 @@
 const name = "Lawal Dauda";
-const hngID = "HNG-00717";
+const hngId = "HNG-00717";
 const email = "omodauda.dl@gmail.com";
 const language = "JavaScript";
 
-const output = "Hello World, this is " + name + " with HNGi7 ID " + hngID + " and email " + email + " using " + language + " for stage 2 task";
+const output = "Hello World, this is " + name + " with HNGi7 ID " + hngId + " and email " + email + " using " + language + " for stage 2 task";
 
 return console.log(output);

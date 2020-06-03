c5ab1a18e5d49c65bf58471070b6963be51b0e36
newhasher <ibrahimadeyemi88@gmail.com>
2020-06-03 13:36:45 +0100

    newhasher-HNG-01855 script

--- scripts/newhasher-HNG-01855.py
+++ scripts/newhasher-HNG-01855.py
@@ -5,4 +5,4 @@ email = "ibrahimadeyemi88@gmail.com"
 HNGi7id = "HNG-01855"
 language = "python"
 
-print ("Hello World, this is", name, "with HNGi7 ID", HNGi7id, "and", email, "using", language, "for stage 2 task")
+print ("Hello World, this is", name, "with HNGi7 ID", HNGi7id, "and email", email, "using", language, "for stage 2 task")

45d574a6dcb57536ca63460337007cac94d78e61
udoh <66221563+udohahanor@users.noreply.github.com>
2020-06-03 18:05:54 +0100

    Delete udoh-HNG-HNG-02216.js

--- scripts/udoh-HNG-HNG-02216.js
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<script language="javascript" type="text/javascript">
-<!--
-	document.write("Hello World, this is Udoh Ahanor with HNG ID HNG-02216 using python for Stage 2 task")
-//-->
-</script>
-</body>
-</html>

6f0d5c528e6d181306841c8a0960e0e1433a42bf
Akindele Taiwo <akindele_taiwo57@yahoo.com>
2020-06-03 12:22:35 +0100

    Taiwodave_HNG_03847 script

---
+++ scripts/Taiwodave_HNG_03847.js
@@ -0,0 +1,10 @@
+var hello = "Hello World";
+var name = "this is Akindele Taiwo with";
+var hngId = "HNG-03847";
+var email = "taiwodavecode@gmail.com";
+var language = "using Javascript for stage 2 task.";
+
+var text = hello + "," + name + " " + hngId + " email address " +email + " " + language;
+
+console.log(text);
+

7a46187a52032939f0fd756037b5b7acd22cbe0a
Omonzejele Jennifer <omonzejelejennifer@gmail.com>
2020-06-03 22:58:56 +0100

    Jenni-HNG-05151 script

---
+++ scripts/Jenni-HNG-05151.js
@@ -0,0 +1,8 @@
+let fullname = 'Omonzejele Jennifer';
+let id = 'HNG-05151';
+let email = 'omonzejelejennifer@gmail.com';
+let lang = 'JavaScript';
+
+
+
+console.log("Hello World, this is " + fullname + " with HNGi7 ID " + id + " and email " + email +  "using " + lang + " for stage 2 task ");

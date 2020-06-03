fe72fb4ef677443f0e2ad700d022c9dec01b437f
joevibanks <joevibanks@gmail.com>
2020-06-03 12:39:22 +0100

    joevi-HNG-01323 script

---
+++ scripts/joevi-HNG-01323.js
@@ -0,0 +1,3 @@
+let headTag = document.createElement("body");
+   headTag = "Hello world, this is Joshua Oguaghamba "+"with HNGi7 ID HNG-01323 "+"and joevibanks@gmail.com "+"using JavaScript for stage 2 task";
+console.log(headTag);

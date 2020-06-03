51221a7aca38298f88339d8bb6c2c8d758a940f6
Oluwaseun Sofoluwe <sofoluweseun@gmail.com>
2020-06-04 00:09:02 +0100

    d18thday-HNG-00677.js

--- scripts/d18thday-HNG-00677.js
+++ scripts/d18thday-HNG-00677.js
@@ -5,7 +5,7 @@ let language = "Javascript"
 let userEmail = "sofoluweseun@gmail.com"
 
 let displayMessage = function(name, ID, email, language,){
-    console.log(`Hello World, this is ${name} with HNGi7 ${ID} and email ${email} using ${language} for stage 2 task`);
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`);
 }
 
 displayMessage(fullName, HNGID, userEmail, language);

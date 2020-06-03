fe767a3b961829976881d026820d55d03d649310
Silhoutte17 <abfatahi.iaf@gmail.com>
2020-06-03 13:55:17 +0100

    silhouette-HNG-05751 script

--- scripts/silhouette-HNG-05751.js
+++ scripts/silhouette-HNG-05751.js
@@ -3,8 +3,6 @@ var myInternId = "05751";
 var myEmail = "abfatahi96@gmail.com";
 var myLanguage = "JavaScript";
 
-var myOutput = "Hello World, this is" + " " + myName + " " +
- "with HNGi7 ID HNG-" + myInternId + " " + "and email" + " " + myEmail +
-    " " + "using" +" "+ myLanguage + " " + "for stage 2 task";
+var myOutput = "Hello World, this is" + " " + myName + " " + "with HNGi7 ID HNG-" + myInternId + " " + "and email" + " " + myEmail + " " + "using" +" "+ myLanguage + " " + "for stage 2 task";
 
 console.log(myOutput);

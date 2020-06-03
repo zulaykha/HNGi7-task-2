8f22d8437cbffb10dd1837c94579374eab5b66d3
TobyCodes <twiseas9ice@gmail.com>
2020-06-03 20:07:50 +0100

    tobyCodes-HNG-01779 script

--- scripts/tobyCodes-HNG-01779.js
+++ scripts/tobyCodes-HNG-01779.js
@@ -1,10 +1,10 @@
 const name = 'Paul Tobiloba';
 const id = 'HNG-01779';
 const email = 'twiseas9ice@gmail.com';
-const language = 'JavaScript';
+const language = 'Javascript';
 
 
 const output = () => {
-    return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+    return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
 }
 console.log(output());

4f907856b7c4f1897296536deb9d2ef57ffbbf9f
joevibanks <joevibanks@gmail.com>
2020-06-03 13:35:35 +0100

    I a correction to the previous push

--- scripts/joevi-HNG-01323.js
+++ scripts/joevi-HNG-01323.js
@@ -1,3 +1,2 @@
-let headTag = document.createElement("body");
-   headTag = "Hello world, this is Joshua Oguaghamba "+"with HNGi7 ID HNG-01323 "+"and joevibanks@gmail.com "+"using JavaScript for stage 2 task";
+let headTag = "Hello world, this is Joshua Oguaghamba with HNGi7 ID HNG-01323 and joevibanks@gmail.com using JavaScript for stage 2 task";
 console.log(headTag);

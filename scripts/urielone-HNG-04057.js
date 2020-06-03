7cd111bcfce1264c85b9ea4c66bd4b6daae49ba0
x3meuriel <x3me7@hotmail.com>
2020-06-03 23:53:28 +0100

    modified my js file urielone-HNG-04057.js

--- scripts/urielone-HNG-04057.js
+++ scripts/urielone-HNG-04057.js
@@ -1,11 +1,10 @@
-
-let tf_urielone = {
-    name: "Ukwueji Okechukwu Emmanuel",
+let urielone = {
+    name: "Ukwueji OKechukwu Emmanuel",
     id: "HNG-04057",
     email: "x3me7@hotmail.com",
     language: "javascript"
 }
 
-let urielone_msg = "Hello World, My name is " + tf_urielone.name + " with HNGi7 id of " + tf_urielone.id + " and email: " + tf_urielone.email + ", i used " + tf_urielone.language + " scripting language for the stage 2 task";
+let msg = "Hello World, this is " + urielone.name + " with HNGi7 ID " + urielone.id + " and email " + urielone.email + " using " + urielone.language + " for stage 2 task";
 
-console.log(urielone_msg);
+console.log(msg); 

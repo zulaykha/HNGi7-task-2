70fc21b942ea0c4cb10217bf488dacdd95c60473
odomfavour <ogochukwuodom@gmail.com>
2020-06-03 11:53:39 +0100

    Ogochuks-HNG-01707 script

---
+++ scripts/Ogochuks-HNG-01707.js
@@ -0,0 +1,13 @@
+const fullName = "Ogochukwu Odom";
+const id = "HNG-01707";
+const language = "JavaScript";
+const email = "ogochukwuodom@gmail.com"
+
+printString(fullName, id, language, email)
+
+function printString(name, id, lang, email) {
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`);
+
+    return 
+}
+

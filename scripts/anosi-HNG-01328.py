f3229063f10b74d31851743b9a1248027ea1a850
anosi-d <unodavid06@gmail.com>
2020-06-03 19:58:10 +0100

    anosi-HNG-01328 script

--- scripts/anosi-HNG-01328.py
+++ scripts/anosi-HNG-01328.py
@@ -1,3 +1,4 @@
+#Script
 first_name = "David"
 last_name = "Unoarumhi"
 hng_id = "HNG-01328"
@@ -5,4 +6,5 @@ email = "unodavid06@gmail.com"
 lang = "Python"
 
 message = "Hello World, this is " + first_name + " " + last_name + " with HNGi7 ID " + hng_id + " and email " + email + " using " + lang + " for stage 2 task"
+
 print(message)

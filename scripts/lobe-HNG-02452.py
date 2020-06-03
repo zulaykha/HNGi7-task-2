0c586520f8964d9f97fec4e25246dffa1fb21abd
lobeserge <lobeserge100@gmail.com>
2020-06-03 16:47:43 +0100

    lobe-HNG-02452 script

---
+++ scripts/lobe-HNG-02452.py
@@ -0,0 +1,8 @@
+def my_details():
+    name = "Lobe Serge"
+    hng_id = "HNG-02452"
+    email = "lobeserge100@gmail.com"
+    language = "Python"
+    print("Hello World, this is "+ name + " with HNGi7 ID "+ hng_id + " and email "+email+" using "+language+" for stage 2 task")
+
+my_details()

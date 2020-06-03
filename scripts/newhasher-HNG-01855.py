20e559631a9d76a9eb17b1886a1b845d3ac10aac
Ibrahim3016 <ibrahimadeyemi88@gmail.com>
2020-06-03 11:00:22 +0100

    newhasher-HNG-01855 script

---
+++ scripts/newhasher-HNG-01855.py
@@ -0,0 +1,8 @@
+# This is a Python script for the HNGi7 stage 2 task
+
+name = "Ibrahim Adeyemi"
+email = "ibrahimadeyemi88@gmail.com"
+HNGi7id = "HNG-01855"
+language = "python"
+
+print ("Hello World, this is", name, "with HNGi7 ID", HNGi7id, "and", email, "using", language, "for stage 2 task")

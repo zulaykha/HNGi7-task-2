58cee39919be7f257d0b54b54a7cd863875b12d0
PreciousOge <61980399+Preciousoge@users.noreply.github.com>
2020-06-03 19:02:15 +0100

    updated Oge-HNG-00932-script

--- scripts/Oge-HNG-00932.js
+++ scripts/Oge-HNG-00932.js
@@ -2,7 +2,7 @@
  let Oge = {
  	fullName: 'Precious Emeka-Nwafor', 
  	HNGi7ID: 'HNG-00932', 
- 	language: 'JavaScript'
+ 	language: 'JavaScript',
  	email: 'preciousoge98@gmail.com'
  }
  console.log(`Hello world, This is ${Oge.fullName} with ${Oge.HNGi7ID} using ${Oge.language}`)

0619614b691675a57f6ad7d3b99d1f5f0ab1a0a7
Maurice Kosisochukwu Oodo <51403047+maurizok@users.noreply.github.com>
2020-06-03 08:29:58 -0600

    maurizo-HNG-03268 script

---
+++ scripts/Maurizo-HNG-03268.js
@@ -0,0 +1,6 @@
+let fullName = "Oodo Maurice Kosisochukwu";
+let ID = "HNG-03268";
+let language = "JavaScript";
+let email = "mauricekosi@gmail.com";
+
+console.log("Hello world, this is " + fullName + " with HNGi7 ID " + ID + " and email " + email + " using " + language + " for stage 2 task");

638ea1849de9f04f2645487ee915c0d9d3966dde
Bamidele OLUWATOSIN <65241457+tomisi@users.noreply.github.com>
2020-06-03 15:35:11 +0100

    bamideletosin00281-HNG-ID.py

---
+++ bamideletosin00281-HNG-ID.py
@@ -0,0 +1,6 @@
+name = "Bamidele Oluwatosin"
+ID = "00281"
+language = "python"
+email_address = "tomisinkemi468@gmail.com"
+output = f"My name is {name} with HNGi7 ID {ID} using {python} for stage 2 task {email_address}."
+print(output)

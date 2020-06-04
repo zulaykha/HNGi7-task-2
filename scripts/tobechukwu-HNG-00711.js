96a76eee20479aa0e1d7069354fb2e2c3beac9d9
Tobechukwu Agbasi <tochihenry28@gmail.com>
2020-06-04 15:53:14 +0100

    adding Tobechukwu's script

---
+++ scripts/tobechukwu-HNG-00711.js
@@ -0,0 +1,8 @@
+const myName = "Tobechukwu Agbasi";
+const myId = "HNG-00711";
+const email = "tochihenry28@gmail.com";
+const language = "JavaScript";
+
+
+
+console.log(`Hello World, this is ${myName} with HNGi7 ID ${myId} and email ${email} using ${language} for stage 2 task`);

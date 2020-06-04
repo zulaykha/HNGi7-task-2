b8d5ee219c46661016b3502aa12a8692a8cd935f
Stanley Umezuruike <manstylo@gmail.com>
2020-06-04 05:19:26 +0100

    CodeHunta-HNG-01135 script

---
+++ scripts/CodeHunta-HNG-01135.py
@@ -0,0 +1,7 @@
+fullName = "Stanley Umezuruike"
+hngID = "HNG-01135"
+language = "Python"
+email = "manstylo@gmail.com"
+message = f"Hello World, this is {fullName} with HNGi7 ID {hngID} using {language} for stage 2 task"
+
+print(message)

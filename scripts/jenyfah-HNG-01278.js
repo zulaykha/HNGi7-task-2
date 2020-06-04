2ac8a442c105a8f9210962d169f5a638b45e8fac
Jennifer Ehiagbonare <osejoseph33@gmail.com>
2020-06-04 20:00:02 +0100

    jenyfah-HNG-01278 script

---
+++ scripts/jenyfah-HNG-01278.js
@@ -0,0 +1,6 @@
+var myName = "Jennifer Ehiagbonare";
+var id = "HNG-01278";
+var language = "JavaScript";
+var email = "osejoseph33@gmail.com";
+
+console.log("Hello World, this is " + myName + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task");

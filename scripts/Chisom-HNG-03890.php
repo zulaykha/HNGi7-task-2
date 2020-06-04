5c0c35e3f3208683040460be6c7695e62872dcc9
CHISOM <chisomdick2@gmail.com>
2020-06-05 00:57:22 +0200

    Add existing file

--- scripts/Chisom-HNG-03890.php
+++ scripts/Chisom-HNG-03890.php
@@ -4,6 +4,6 @@ $ID = "HNG-03890";
 $email = "chisomdick2@gmail.com";
 $language = "php";
 
-echo ("Hello World, This is $fullname with HNGi7 ID $ID, using $language for stage 2 task.$email")
+echo ("Hello World, This is $fullname with HNGi7 ID $ID using $language for stage 2 task.")
 
 ?>

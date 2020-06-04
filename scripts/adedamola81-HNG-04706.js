77f899f2a1481f0ca8979e1f653c97b41ba274a1
Adedamola Onasanya <adedamolaonasanya@gmail.com>
2020-06-04 10:04:34 +0100

    adedamola81-HNG-04706 script

--- scripts/adedamola81-HNG-04706.js
+++ scripts/adedamola81-HNG-04706.js
@@ -1,3 +1,3 @@
 console.log(
-  "Hello World, This is Onasanya Adedamola with HNG-04706 and my email adedamolaonasanya@gmail.com using javascript for stage 2 task"
+  "Hello World, This is Onasanya Adedamola with HNGi7 ID HNG-04706 and email adedamolaonasanya@gmail.com using javascript for stage 2 task"
 );

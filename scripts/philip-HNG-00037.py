f1b99e7a6324f22e1bdf7b1e90ebb541bb51ee84
Philip Okiokio <philipokiokio@gmail.com>
2020-06-04 09:51:06 +0100

    philip-HNG-00037 script

---
+++ scripts/philip-HNG-00037.py
@@ -0,0 +1,12 @@
+"""Second task that qualifies an intern to stage 2."""
+
+def task(fullname, id, lang, email):
+    return print(f"Hello world, this is {fullname} with HNGi7 ID {id} and email {email} using {lang} for stage 2 task.", flush= True)
+
+fullname= 'Philip Ireoluwa Okiokio'
+id= 'HNG-00037'
+lang= 'python'
+email= 'philipokiokiocodes@gmail.com'
+
+
+task(fullname,id,lang, email)

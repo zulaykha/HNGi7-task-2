207bd8c3ec0915037b32e28d4ae36cab5d71cf2e
Anolue Ekene Samuel <kenelight4u@gmail.com>
2020-06-04 19:35:05 +0100

    kenelight4u-HNG-05513 script

---
+++ scripts/kenelight4u-HNG-05513.js
@@ -0,0 +1,6 @@
+let name = 'Ekene Samuel Anolue';
+let id = 'HNG-05513';
+let email = 'kenelight4u@gmail.com';
+let language = 'JavaScript';
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);

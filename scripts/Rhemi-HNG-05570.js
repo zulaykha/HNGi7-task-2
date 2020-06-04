4005a3c8a440e9ca77089dddc5a16c11048ca6e6
Rhemi7 <adelefatai@yahoo.com>
2020-06-04 12:29:21 +0100

    Rhemi-HNG-05570 script

---
+++ scripts/Rhemi-HNG-05570.js
@@ -0,0 +1,7 @@
+const myFullName = "Adele Fatai Aderemilekun";
+const myHNGId = "HNG-05570";
+const myEmail = "adelefatai@yahoo.com";
+const myLanguage = "Javascript";
+const stage = "2";
+
+console.log(`Hello World, this is ${myFullName} with HNGi7-ID ${myHNGId} and Email ${myEmail} using ${myLanguage} for Stage ${stage} task`);

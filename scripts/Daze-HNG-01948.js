b71e74058c319d966a051a58c88055f3ea7e3f19
Daze <52837983+frankly-frankie@users.noreply.github.com>
2020-06-04 21:14:14 +0100

    Update Daze-HNG-01948.js

--- scripts/Daze-HNG-01948.js
+++ scripts/Daze-HNG-01948.js
@@ -4,5 +4,6 @@ const eMail = "frankpeterani@gmail.com";
 const pLang = "JavaScript"
 
 console.log(
-    `Hello World, This is ${fullName} with HNG i7 ID ${boardId} and email ${eMail} using ${pLang} for Stage 2 task.`) 
+    `Hello World, this is ${fullName} with HNGi7 ID ${boardId} and email ${eMail} using ${pLang} for Stage 2 task`
+);
     

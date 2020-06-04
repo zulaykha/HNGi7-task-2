254bfd4caa479b15401af1129ee06482426cc046
mofiyin2003 <66219083+mofiyin2003@users.noreply.github.com>
2020-06-04 21:06:24 +0100

    HNG-01273

---
+++ scripts/mofiyin-HNG-01273.js
@@ -0,0 +1,7 @@
+//HNG Task 2
+var name = "Mofiyinfoluwa Olaniyi";
+var HngId = "HNG-01273";
+var language = "Javascript";
+var myMail = "olaniyimofiyinfoluwa3@gmail.com"
+
+console.log("Hello World, this is "  + name +  " with HNGi7 ID "  + HngId + "and email " + myMail + " using " + language + " for stage 2 task ");

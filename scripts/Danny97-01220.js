eb9844eab6071db2499c27dad125575565761940
Ayodeji97 <danielayodeji97@gmail.com>
2020-06-04 03:10:19 +0100

    Danny97-HNG-01220 script

---
+++ scripts/Danny97-01220.js
@@ -0,0 +1,5 @@
+const fullName = 'Daniel Ayodeji';
+const id_number = 'HNG-01220';
+const language = 'Javascript';
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id_number} using ${language} for stage 2 task`);

c356ff37a945a1b44b8cee6825407fbbdffe42ff
ogunbiyiolorunwa <ogunbiyiolorunwa1@gmail.com>
2020-06-04 11:12:07 +0100

    Leocoded-HNG-00433 script

---
+++ scripts/Leocoded-HNG-00433.js
@@ -0,0 +1,6 @@
+let fullName = "Olorunwa Ogunbiyi";
+let hngid = "HNG-00433";
+let email = "ogunbiyiolorunwa1@gmail.com";
+let stack = "Javascript";
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngid} and email ${email} using ${stack} for stage 2 task`);

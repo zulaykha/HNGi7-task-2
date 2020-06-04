213e6c8122307bd760f300dcbcfb2aef3e8fe8c3
Dancanojijo <66280137+Dancanojijo@users.noreply.github.com>
2020-06-04 23:31:42 +0000

    Update dancanojijo-HNG-05033.py

--- scripts/dancanojijo-HNG-05033.py
+++ scripts/dancanojijo-HNG-05033.py
@@ -24,7 +24,7 @@
 
 
 def main(args):
-    print("Hello World,this is Dancan Ojijo with HNGi7 ID HNG-05033 and email is ojidat1@gmail.com using python for stage 2 task")
+    print("Hello World,this is Dancan Ojijo with HNGi7 ID HNG-05033 and email ojidat1@gmail.com using python for stage 2 task")
 
 if __name__ == '__main__':
     import sys

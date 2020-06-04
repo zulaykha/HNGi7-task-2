160a15df83eecd61741f52ab184763766bd18f18
Tura <adunola142@gmail.com>
2020-06-04 11:50:17 +0100

    Task Completed

---
+++ scripts/Tura-HNG-02324.js
@@ -0,0 +1,5 @@
+var name = "Adeyemi Misturah";
+var email = "adunola142@gmail.com";
+var id = "HNG-02324";
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using Javascript for stage 2 task.");

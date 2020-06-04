ce18ca4f8df97af9053a159e549f6897524cbb43
Olaoluwa IBUKUN <ibukundaniel402@yahoo.co.uk>
2020-06-04 21:43:34 +0000

    Joy_402-HNG-03641 script

---
+++ scripts/Joy_402-HNG-03641.js
@@ -0,0 +1,9 @@
+const myName ='Fasunhanmi Pelumi';
+const HNGi7ID ='HNG-03641';
+const language ='JavaScript';
+const email='faslusky@gmail.com';
+
+
+const Data = `Hello World, this is ${myName} with HNGi7 ID ${HNGi7ID} and email ${email} using ${language} for stage 2 task`
+
+console.log(Data);

6ee7e164c6b612eed49fbfb22526ed9ef2e46fe9
forolami <iruleadeleye@gmail.com>
2020-06-04 02:48:51 -0700

    forolami-HNG-01274 python script

---
+++ scripts/forolami-HNG-01274.py
@@ -0,0 +1,6 @@
+def script_name(name, HNGid, email):
+    print("Hello World,", ""," this is","", name , "" ,
+          "with", HNGid, "", "and", email,"", "using python for stage 2 task")
+          
+script_name("Adeleye Tahir", "HNG-01274", "iruleadeleye@gmail.com")
+

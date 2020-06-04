d33c0e8b65832580bbae55cc3a85538b51424d70
youthtrouble <47859940+youthtrouble@users.noreply.github.com>
2020-06-04 09:52:04 +0100

    ayodeji-HNG-00146 script

---
+++ scripts/ayodeji-HNG-00146.py
@@ -0,0 +1,7 @@
+fullname = "Deji Ajibola"
+id = "HNG-00146"
+language = "Python"
+email = "harjhibolar@gmail.com"
+
+
+print("Hello world, this is %s with HNGi7 ID %s and email %s using %s for stage 2 task."%(fullname, id, email, language), flush = True)

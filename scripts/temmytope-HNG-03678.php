0f462fe4145c3a9e393b35c0deef1065bd26593b
temmytope88 <fakiletemitope@gmail.com>
2020-06-04 23:49:21 +0100

    correction

--- scripts/temmytope-HNG-03678.php
+++ scripts/temmytope-HNG-03678.php
@@ -1,3 +1,3 @@
 <?php
-  print("Hello world, this is Fakile Temitope with HNGi7 ID HNG-03678 and email fakiletemitope@gmail.com using php for stage 2 task.");
+  print("Hello world, this is Fakile Temitope with HNGi7 ID HNG-03678 and email fakiletemitope@gmail.com using php for stage 2 task");
 ?>

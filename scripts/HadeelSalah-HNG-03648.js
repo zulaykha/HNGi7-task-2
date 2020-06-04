2c59bec6c556322c41e9c675e6db97126212b515
hadeel salah <hadeelsalah80@gmail.com>
2020-06-04 14:23:28 +0300

    hadeel-HNG-03648 script

---
+++ scripts/HadeelSalah-HNG-03648.js
@@ -0,0 +1,10 @@
+const Name = "Hadeel Salah",
+     Id = "HNG-03648",
+     Email = "hadeelsalah80@gmail.com", 
+     Language = "JavaScript", 
+     Result = "My name is" + Name + " and my HNGi7 ID is " + Id + ", my email is " + Email + "  this is done using " + Language + " for task 2 :)"; 
+     
+     
+     
+     
+console.log(Result); 

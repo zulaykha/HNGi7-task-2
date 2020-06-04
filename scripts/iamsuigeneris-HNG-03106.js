ad3e8bc6c503c99cbe35953ba46650966df56340
iamsuigeneris <irewolelanre@gmail.com>
2020-06-04 18:51:57 +0100

    iamsuigeneris-HNG-03106 script

---
+++ scripts/iamsuigeneris-HNG-03106.js
@@ -0,0 +1,10 @@
+// Printing out out my personal details
+
+const details = {
+    "name":"Lanre Irewole",
+    "id":"HNG-03106",
+    "email":"irewolelanre@gmail.com",
+    "course":"JavaScript"
+}
+
+console.log(`Hello world,this is ${details.name} with HNGi7 ID ${details.id} and email ${details.email} using ${details.course} for stage 2 task`);

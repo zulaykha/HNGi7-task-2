b4e851aca7c11dcc53d86e9fda0385fbc8b4ee68
Aderonke Odushola <aderonkediana@gmail.com>
2020-06-04 18:01:22 +0100

    ronnycrown-HNG-00477 script

--- scripts/ronnycrown-HNG-00477.js
+++ scripts/ronnycrown-HNG-00477.js
@@ -1,4 +1,4 @@
-const greet = "Hello World, this is Aderonke Diana Odushola with HNGi7 ID HNG-00477 and email aderonkediana@gmail.com using Javascript for stage 2 task.";
+const greet = "Hello World, this is Aderonke Diana Odushola with HNGi7 ID HNG-00477 and email aderonkediana@gmail.com using Javascript for stage 2 task";
 function status(){
   return console.log(greet);
 }

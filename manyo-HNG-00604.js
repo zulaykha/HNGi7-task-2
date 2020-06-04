50e71400f2a40879eda83f7f614e8cbbf2ef3c21
Tomiwa Ajayi <ayooluwatomiwaajayi@gmail.com>
2020-06-04 12:33:27 +0100

    Revert "Create manyo-HNG-00604 script"

--- manyo-HNG-00604.js
+++
@@ -1,7 +0,0 @@
-function helloWorld () {
-return "Hello world, this is Judith Ogar with HNGi7 ID HNG-00604
-    and email ogarmanyo4@gmail.com using JavaScript for
-  stage 2"
-}
- const result = helloWorld ()
-  console.log(result)

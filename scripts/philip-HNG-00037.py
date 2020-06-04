c8a2980fec5ac04a87ffaeaa6b94027fb1e4ba6f
Philip Okiokio <philipokiokio@gmail.com>
2020-06-04 13:22:26 +0100

    philipokiokio-HNG-00037 script

--- scripts/philip-HNG-00037.py
+++ scripts/philip-HNG-00037.py
@@ -1,4 +1,4 @@
-"""Second task that qualifies an intern to stage 2."""
+"""This task that qualifies an intern to stage 2."""
 
 def task(fullname, hngid, lang, email):
     return print(f"Hello World, this is {fullname} with HNGi7 ID {hngid} and email {email} using {lang} for stage 2 task", flush= True)

2d437917ab6743ccd773898154f9e441417ed42b
Preciousoge <61980399+Preciousoge@users.noreply.github.com>
2020-06-04 17:30:00 +0100

    Corrected HNG-00932 script

--- scripts/Oge-HNG-00932.js
+++ scripts/Oge-HNG-00932.js
@@ -5,4 +5,4 @@
  	language: 'JavaScript',
  	email: 'preciousoge98@gmail.com'
  }
- console.log(`Hello world, This is ${Oge.fullName} with ${Oge.HNGi7ID} and ${Oge.email} using ${Oge.language} for stage 2 task`)
+ console.log(`Hello world, This is ${Oge.fullName} with HNGi7 ID ${Oge.HNGi7ID} and email ${Oge.email} using ${Oge.language} for stage 2 task`)

10725d2e1a28b9a552f8a2a42ceab920272a3c72
Philip Okiokio <philipokiokio@gmail.com>
2020-06-04 09:58:12 +0100

    Edited ID since it is a keyword in python

--- scripts/philip-HNG-00037.py
+++ scripts/philip-HNG-00037.py
@@ -1,10 +1,10 @@
 """Second task that qualifies an intern to stage 2."""
 
 def task(fullname, id, lang, email):
-    return print(f"Hello world, this is {fullname} with HNGi7 ID {id} and email {email} using {lang} for stage 2 task.", flush= True)
+    return print(f"Hello world, this is {fullname} with HNGi7 ID {ID} and email {email} using {lang} for stage 2 task.", flush= True)
 
 fullname= 'Philip Ireoluwa Okiokio'
-id= 'HNG-00037'
+ID= 'HNG-00037'
 lang= 'python'
 email= 'philipokiokiocodes@gmail.com'
 

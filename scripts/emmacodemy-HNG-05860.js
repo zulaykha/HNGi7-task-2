4dc83ee911af00624a3fecfa3cc9ec677a885f0b
emmacodemy <eayodeleof18@gmail.com>
2020-06-04 18:26:59 +0100

    updated emmacodemy-HNG-05860 task script

--- scripts/emmacodemy-HNG-05860.js
+++ scripts/emmacodemy-HNG-05860.js
@@ -1,9 +1,10 @@
 const name = "Ayodele Emmanuel";
 const ID = "HNG-05860";
 const language = "JavaScript";
+const email = "codedemystifier@gmail.com";
 
-const displayInternInfo = (internName,internID,languageUsed) => {
-    return `Hello World, this is ${name} with HNGi7 ID ${ID} using ${language} for stage 2 task`
+const displayInternInfo = (internName,internID,languageUsed,internEmail) => {
+    return `Hello World, this is ${internName} with HNGi7 ID ${internID} and email ${internEmail} using ${languageUsed} for stage 2 task`;
 };
 
-console.log(displayInternInfo(name,ID,language));
+console.log(displayInternInfo(name,ID,language,email));

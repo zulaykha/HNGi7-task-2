7893fc0d773af9549a25e3438b982867c45afaa2
OLALEKAN NATHANIEL  ASAAJU <52868184+sbpythonista@users.noreply.github.com>
2020-06-04 17:01:57 +0100

    PR from smartbrain-HNG-05813

---
+++ scripts/smartbrain_HNG_05813.py
@@ -0,0 +1,15 @@
+
+#Stage 2 task
+#global variable
+full_name = "Asaaju Olalekan Nathaniel"
+Id = "05813"
+lang = "python"
+stage = "stage2 task."
+statement = "Hello World,"
+e_mail = "asaajulekan@gmail.com"
+def python_script():
+    return  " " +statement + " This is " + full_name +  " with HNGi7\n ID: HNG-" + Id + " and email: " + e_mail+ " using " + lang + " for "  + stage
+
+result = python_script()
+print(result)
+    

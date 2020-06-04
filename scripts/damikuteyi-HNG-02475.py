d5047b031141bbe3025417a2ce688405f93f029c
Damilola Kuteyi <damilola.kuteyi@crealytics.com>
2020-06-04 20:34:23 +0200

    damikuteyi-HNG-02475 script

--- scripts/damikuteyi-HNG-02475.py
+++ scripts/damikuteyi-HNG-02475.py
@@ -2,5 +2,5 @@ first_name = 'Damilola'
 last_name = 'Kuteyi'
 
 email = 'kuteyidamilola@gmail.com'
-message = f'My name is {first_name} {last_name}, and my email is {email}, and I am using Python for this task'
+message = f'Hello World, this is {first_name} {last_name} with HNGi7 ID HNG-02475 and email {email} using Python for stage two task'
 print(message)

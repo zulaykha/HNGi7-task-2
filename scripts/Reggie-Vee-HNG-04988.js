d0c3535ed2d9c263b213f352157e0f04471ff58d
Veronica <veronicatomilola@gmail.com>
2020-06-04 23:33:40 +0100

    fix script fail bug

--- scripts/Reggie-Vee-HNG-04988.js
+++ scripts/Reggie-Vee-HNG-04988.js
@@ -4,4 +4,4 @@ let email = "oolubunmiregina@gmail.com";
 let language = "Javascript";
 
 
-console.log ("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task.")
+console.log ("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task")

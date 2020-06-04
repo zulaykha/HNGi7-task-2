0683fd4aa93b976a0a069d404add774db5a9bfd7
ChuXun <suliat7@gmail.com>
2020-06-04 13:57:10 +0100

    ChuXun-HNG-00607 script

---
+++ ChuXun-HNG-00607.js
@@ -0,0 +1,9 @@
+intern={
+    "firstName":"Zulaykha",
+    "lastName":"Sulaiman",
+    "ID":"HNG-00607",
+    language: "javascript",
+    email:"suliat7@gmail.com"
+};
+
+console.log("Hello World, this is zulaykha sulaiman with HNGi7 ID:HNG-00607 and email:suliat7@gmail.com using javascript for stage2 task");

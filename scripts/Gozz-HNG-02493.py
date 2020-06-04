9f3a42d2aa664027443a8eec5e689bb94553b2df
Gozz-Sand <ajachigozie31@gmail.com>
2020-06-04 09:38:58 +0100

    Gozz-HNG-02493

--- scripts/Gozz-HNG-02493.py
+++ scripts/Gozz-HNG-02493.py
@@ -5,4 +5,4 @@ ID = 'HNG-02493'
 email = 'ajachigozie31@gmail.com'
 lang = 'Python'
 
-print('Hello World, this is', fullname,'with HNGi7 ID', ID,'and email', email,'using', lang, "for stage 2 task")
+print('Hello World, this is', fullname, 'with HNGi7 ID', ID, 'and email', email, 'using', lang, "for stage 2 task")

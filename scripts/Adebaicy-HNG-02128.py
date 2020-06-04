79d98d654bbd6597086a090e81dfeb0396def39c
* <Adebaicy>
2020-06-04 14:53:56 +0100

    Adebaicy-HNG-02128 script

---
+++ scripts/Adebaicy-HNG-02128.py
@@ -0,0 +1,5 @@
+def callhng():
+	return "Hello World, this is Adejumo Oluwapelumi Oluwaseyi with HNGi7 ID HNG-02128 using python for stage 2 task. adebaice@gmail.com"
+
+b = callhng()
+print(b, flush = True)

1ceaef789459ebf5fcac823d8e94be1e52a8217e
KwennB <60541622+kwennB@users.noreply.github.com>
2020-06-05 11:40:03 +0100

    kwennb-HNG-01909-updated-script

--- scripts/kwennb-HNG-01909.js
+++ scripts/kwennb-HNG-01909.js
@@ -4,4 +4,4 @@ let language = "Javascript";
 let email = "anyebeblessing737@yahoo.com";
 
 
-console.log(`Hello  World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`);

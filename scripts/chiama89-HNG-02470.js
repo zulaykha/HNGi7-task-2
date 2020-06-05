4199e67b0591db289fd6e9a6879777a44fd7d268
chiama89 <uwaezuoke.chiamaka@yahoo.com>
2020-06-05 08:19:08 +0100

    removed full stop

---
+++ scripts/chiama89-HNG-02470.js
@@ -0,0 +1,9 @@
+
+const id = "HNG-02470";
+const fullName = "Chiamaka Uwaezuoke";
+const language = "JavaScript";
+const email = "uwaezuoke.chiamaka@yahoo.com";
+
+const details = `Hello World, this is ${fullName} with HNGi7 ID ${id} and ${email} using ${language} for stage 2 task`;
+
+console.log(details);

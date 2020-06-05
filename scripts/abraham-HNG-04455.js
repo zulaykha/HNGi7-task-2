20fe08524f9737171dcf6140f3266c8c1ce43428
Abraham <ekekhorabraham@gmail.com>
2020-06-05 17:26:59 +0100

    Abraham-HNG-04455 script

--- scripts/abraham-HNG-04455.js
+++ scripts/abraham-HNG-04455.js
@@ -4,6 +4,6 @@ var genLang = "JavaScript";
 var email = "ekekhorabraham@gmail.com"
 
 var fullId = "Hello World, this is" + " " + genName + " " + "with HNGi7 ID" + " "+ genId + " " +"and email"
-+" "+email+" "+ "using" +" "+ genLang + " " + "for stage 2 task."
++" "+email+" "+ "using" +" "+ genLang + " " + "for stage 2 task"
 
 console.log(fullId);

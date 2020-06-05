2bf9af4b07000b11e42bde72c8b445c2d1479db0
Idris Idris <49335945+IdrisAdeyemi01@users.noreply.github.com>
2020-06-05 11:45:37 +0100

    Update Idris_idris-HNG-05502.py

--- scripts/Idris_idris-HNG-05502.py
+++ scripts/Idris_idris-HNG-05502.py
@@ -1,5 +1,1 @@
-name = 'Idris'
-email = 'idrisade.eedris@gmail.com'
-id = 'HNG-05502'
-
-print('Hello World, this is Idris Idris Adeyemi with HNGi7 ID HNG-05502 using python for stage 2 task')
+print('Hello World, this is Idris Adeyemi Idris with HNGi7 ID HNG-05502 using Python for stage 2 task')

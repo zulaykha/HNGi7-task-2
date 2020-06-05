9905397eb40bc910de50b1a0616d8b85932a627b
elkemas <ellakemas@gmail.com>
2020-06-05 17:57:55 +0100

    Noela-HNG-06083 script

---
+++ Noela-HNG-06083.js
@@ -0,0 +1,9 @@
+
+const name= "Uchechi Ogbonna";
+const id= "HNG-06083";
+const email="ellakemas@gmail.com";
+const lang= "JavaScript";
+
+let output = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`;
+
+console.log(output);

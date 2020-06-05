40fbe99cd049ce6d653884ee108982611b6a2c45
Oneharry <harryofficialchidi@yahoo.com>
2020-06-05 07:44:14 +0100

    modified Oneharry-HNG-02652 script

--- scripts/Oneharry-HNG-02652.js
+++ scripts/Oneharry-HNG-02652.js
@@ -1,10 +1,6 @@
-function hngTask() {
-    let myFullName = "Ezugwu Harrison";
-    let myHngId = "HNG-02652";
-    let lang = "Javascript"
-    let myEmail = "harryofficialchidi@yahoo.com"
-    let hngBioData = `Hello World, this is ${myFullName} with HNGi7 ID ${myHngId}
-and email ${myEmail} using ${lang} for stage 2 task`;
-    return hngBioData
-} 
-console.log(hngTask());
+let myFullName = "Ezugwu Harrison";
+let myHngId = "HNG-02652";
+let lang = "Javascript";
+let myEmail = "harryofficialchidi@yahoo.com";
+console.log(`Hello World, this is ${myFullName} with HNGi7 ID ${myHngId}
+and email ${myEmail} using ${lang} for stage 2 task`);

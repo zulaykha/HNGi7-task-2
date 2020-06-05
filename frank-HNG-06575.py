a15f6f25774fc38d031e9b6526d9595b9f5bba29
osas2211 <39238159+osas2211@users.noreply.github.com>
2020-06-05 19:22:05 +0100

    Add files via upload

---
+++ frank-HNG-06575.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  4 12:04:38 2020
+
+@author: Osariemen Osaretin Frank
+"""
+
+class HNGi7(object):
+    def __init__(self, name, hngi7_id, lang):
+        self.name = name
+        self.id = hngi7_id
+        self.lang = lang
+    def output(self):
+        return 'Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.'.format(self.name, self.id, self.lang)
+
+details = HNGi7('Osaretin Frank', 'HNG-06575', 'Python')
+print(details.output())
+        
+
+

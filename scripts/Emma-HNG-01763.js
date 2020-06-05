c34d572fb3a95e7166c74cd7aedd3736376f6321
Tomiwa97 <ajayitomiwa71@gmail.com>
2020-06-05 20:03:39 +0100

    fix file-tree

--- scripts/Emma-HNG-01763.js
+++ scripts/Emma-HNG-01763.js
@@ -5,6 +5,5 @@ const info = {
   language: "JavaScript",
 };
 
-const greeting = `Hello World, this is ${info.fullName} with HNGi7 ID ${info.hngID} and email ${myEmail}using ${info.language} for stage 2 task`;
-
-console.log(greeting);
+const output = `Hello World, this is ${info.fullName} with HNGi7 ID, ${info.hngID} and email, ${info.myEmail} using ${info.language} for stage 2 task`;
+console.log(output);

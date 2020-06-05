bbe1dddcf792dbb664d4194a78066c53b4958164
osas2211 <39238159+osas2211@users.noreply.github.com>
2020-06-05 19:42:23 +0100

    Update frank-HNG-06575.py

--- frank-HNG-06575.py
+++ frank-HNG-06575.py
@@ -6,12 +6,13 @@ Created on Fri Jun  4 12:04:38 2020
 """
 
 class HNGi7(object):
-    def __init__(self, name, hngi7_id, lang):
+    def __init__(self, name, hngi7_id, email, lang):
         self.name = name
         self.id = hngi7_id
         self.lang = lang
+        self.email = email
     def output(self):
-        return 'Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.'.format(self.name, self.id, self.lang)
+        return 'Hello World, this is {} with HNGi7 ID {} and {} using {} for stage 2 task.'.format(self.name, self.id, self.email, self.lang)
 
 details = HNGi7('Osaretin Frank', 'HNG-06575', 'Python')
 print(details.output())

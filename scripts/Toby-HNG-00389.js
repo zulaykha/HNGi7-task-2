e81921ec5214ded0cddf6dc74a38638b802f11e8
unknown <davidolubusoye@gmail.com>
2020-06-05 07:49:56 +0100

    toby-HNG-00389 script

--- scripts/Toby-HNG-00389.js
+++ scripts/Toby-HNG-00389.js
@@ -2,6 +2,6 @@ let name = "David Olubusoye",
     hngId = "HNG-00389",
     language = "JavaScript",
     email = "davidolubusoye@gmail.com",
-    string = `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
+    string = `Hello World, this is ${name} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`;
 
 console.log(string);

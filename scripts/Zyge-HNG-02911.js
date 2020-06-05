902d41ed240874d375cccb2830588abe7bcf0632
ZygeSam <54424855+ZygeSam@users.noreply.github.com>
2020-06-05 19:42:19 +0100

    Zyge-HNG-02911 script

---
+++ scripts/Zyge-HNG-02911.js
@@ -0,0 +1,6 @@
+const name = "Sakinbarnes Ayomikun Samuel";
+const id = "HNG-02911";
+const lang = "JavaScript";
+const email = "sakinbarnessamuel@gmail.com";
+message=`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`;
+console.log(message);

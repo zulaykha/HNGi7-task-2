29ee032653169220cc1081bcebb5ff3d7f6d9804
wisedman10 <66228015+wisedman10@users.noreply.github.com>
2020-06-05 19:23:03 +0100

    Add files via upload

--- scripts/obinnaobidiegwu-HNG-03050.py
+++ scripts/obinnaobidiegwu-HNG-03050.py
@@ -1,2 +1,2 @@
 #!/obi85/python3
-print("Hello World, this is Obinna Obidiegwu with HNGi7 ID HNG-03050 and email testwise85@gmail.com using Python for stage 2 task")
+print("Hello World, this is Obinna Obidiegwu with HNGi7 ID HNG-03050 and email obinna.obidiegwu@gmail.com using Python for stage 2 task")

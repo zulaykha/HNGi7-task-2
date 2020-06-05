13f2307c6bd24faf5da5ecd5e4c296281c05346d
jhaameehu <jamiuazeez49@gmail.com>
2020-06-04 19:59:11 -0700

    Jamiu-HNG-01243 script

---
+++ scripts/Jamiu-HNG-01243.js
@@ -0,0 +1,18 @@
+const hng = [{
+    welcomeNote: 'Hello World',
+    name: 'Azeez Jamiu Adewale',
+    internshipId: 'HNG-01243',
+    emailAddress: 'jamiuazeez49@gmail.com',
+    language: 'Javascript'
+}];
+
+const task = details => {
+
+    const currentDetails = details.map(data => {
+        return ` ${data.welcomeNote}, this is ${data.name}  with HNGi7 ID ${data.internshipId}, and email address ${data.emailAddress} using ${data.language} for stage 2 `;
+    });
+
+    return currentDetails;
+}
+
+console.log(task(hng));

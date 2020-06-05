ee55c61d868e6a8924b112e24830eeca6243421d
aishaabaji <66283375+aishaabaji@users.noreply.github.com>
2020-06-05 15:54:49 +0100

    Update aishaabaji-HNG-05169.py

--- scripts/aishaabaji-HNG-05169.py
+++ scripts/aishaabaji-HNG-05169.py
@@ -1,1 +1,1 @@
-print("Hello World, this Hauwa Abaji with HNGi7 ID HNG-05169 and email aishaabaji@gmail.com using Python for stage 2 task")
+print("Hello World, this is Hauwa Abaji with HNGi7 ID HNG-05169 and email aishaabaji@gmail.com using Python for stage 2 task")

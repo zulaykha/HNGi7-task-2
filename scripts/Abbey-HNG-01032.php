f85b115e136d983449c0abdadbdeea441b194bde
abbey <dolapo140@gmail.com>
2020-06-05 10:32:17 +0100

    changed scripts/Abbey-HNG-01032.php

--- scripts/Abbey-HNG-01032.php
+++ scripts/Abbey-HNG-01032.php
@@ -1,11 +1,11 @@
 <?php
-    $fullname = "Awoniyi Abiodun Abbey";
+    $full_name = "Awoniyi Abiodun Abbey";
     $ID = "HNG-01032";
     $email = "dolapo140@gmail.com";
     $language = "php";
 
 
-    $output = "Hello world, this is Abbey with HNG-01032 using php for stage 2 dolapo140@gmail.com";
+    $output = "Hello world this is"  . $full_name . "with HNG" .$ID ."and email".$email . "using".$language. "for stage 2 ";
 
     echo ($output);
 

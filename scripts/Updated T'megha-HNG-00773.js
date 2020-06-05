3b6b9895b646f08a10e3154b78bc05faf92791c2
temitope akinmegha <48892912+temitopeakin1@users.noreply.github.com>
2020-06-05 19:17:09 +0100

    Update and rename scripts/Updated T'megha-HNG-00773.js to scripts T'megha-HNG-00773.js

--- scripts/Updated T'megha-HNG-00773.js
+++
@@ -1,7 +0,0 @@
-let newintern = (name, id, email, language) => {
-  
-  
-  letgreetings = `Hello World,this is ${name}with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task';
-    return greetings;
-  } 
-  console.log(newintern("Akinmegha Temitope", "HNG-00773", "temitopeakinmegha@gmail.com, JavaScript));

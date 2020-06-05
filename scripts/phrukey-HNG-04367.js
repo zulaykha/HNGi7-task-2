de93ff5217ee0ac74326b869a322735256f5a7a9
Phrukey <muhdfaruuq@gmail.com>
2020-06-05 16:38:14 +0100

    phrukey-HNG-04367 script another correction

--- scripts/phrukey-HNG-04367.js
+++ scripts/phrukey-HNG-04367.js
@@ -1,8 +1,8 @@
 const name= "Sadiq Faruq";
 const id = "HNG-04367";
-const mail = "muhdfaruuq@gmail.com";
+const email = "muhdfaruuq@gmail.com";
 const languague = "Javascript";
 
-let output = `Hello world, this is ${name} with HNGi7 ID ${id} using ${languague} for the stage 2 task`
+let output = `Hello world, this is ${name} with HNGi7 ID ${id} using ${languague} for stage 2 task`
 
 console.log(output)

42520e1c87820ad73b525f480a7c03bbfc977fac
Greennsikak <57267256+Greennsikak@users.noreply.github.com>
2020-06-05 17:58:42 +0100

    Update amy-HNG-03144.js

--- scripts/amy-HNG-03144.js
+++ scripts/amy-HNG-03144.js
@@ -1,3 +1,6 @@
-const amarachi = "Hello World, this is Amarachi Ihuoma with HNG";
-const javascripts = "ID-HNG03144 using Javascript for stage 2 task. ihuomaamarachia@gmai.com";
-console.log(amarachi+javascript);
+const name = "Amarachi Ihuoma";
+const hngId = "HNG-03144";
+const email = "ihuomaamarachia@gmail.com";
+const language = "Javascript";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`);

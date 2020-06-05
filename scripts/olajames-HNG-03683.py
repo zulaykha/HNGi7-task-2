2b3b7ca5ca90e6971a914993aa031380cd11159d
james afolaranmi <olaoluwaafolaranmi@gmail.com>
2020-06-05 05:58:16 +0100

    add to existing file

--- scripts/olajames-HNG-03683.py
+++ scripts/olajames-HNG-03683.py
@@ -1,7 +1,7 @@
 hngid = 'HNG-03683'
 name = "Afolaranmi James Olaoluwa"
 email = "olaoluwaafolaranmi@gmail.com"
-language = "python"
+language = "Python"
 def team_task():
     print(f"""Hello World, this is {name} with HNGi7 ID {hngid} and email {email} using {language} for stage 2 task""")
 

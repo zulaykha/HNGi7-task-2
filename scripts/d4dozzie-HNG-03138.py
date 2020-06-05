f505455f81763483ad31969bb2b991ef365c58c8
d4dozzie <dozziechidozie@yahoo.com>
2020-06-05 17:48:34 +0200

    feat: updated script

---
+++ scripts/d4dozzie-HNG-03138.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 16:44:40 2020
+
+@author: Madukwe Chidozie
+"""
+
+params = {
+    'name': 'Madukwe Chidozie',
+    'ID': 'HNG-03138',
+    'language': 'Python',
+    'email': 'dozziechidozie@yahoo.com'
+}
+
+if __name__ == "__main__":
+    print("Hello World, this is " + params['name'] + " with HNGi7 ID " + params['ID'] + " and email " + params['email'] +" using " + params['language']+" for stage 2 task.")

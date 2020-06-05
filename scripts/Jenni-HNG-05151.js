ba48dcfc38442669eda0057798f1c830423c63e4
Omonzejele Jennifer <omonzejelejennifer@gmail.com>
2020-06-05 06:38:15 +0100

    Jenni-HNG-05151 HNG i would update

--- scripts/Jenni-HNG-05151.js
+++ scripts/Jenni-HNG-05151.js
@@ -5,4 +5,4 @@ let lang = 'JavaScript';
 
 
 
-console.log("Hello World, this is " + fullname + " with HNGi7 ID " + id + " and email " + email +  "using " + lang + " for stage 2 task ");
+console.log("Hello World, this is " + fullname + " with HNGi7 ID " + id + " and email " + email +  " using " + lang + " for stage 2 task ");

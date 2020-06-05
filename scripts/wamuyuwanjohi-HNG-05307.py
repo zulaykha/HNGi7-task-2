f928a09dd83f35a477066a8ae60190d3e5647426
unknown <wamuyuwanjohi97@gmail.com>
2020-06-05 07:47:47 +0300

    wamuyuwanjohi HNG-05307 script

--- scripts/wamuyuwanjohi-HNG-05307.py
+++ scripts/wamuyuwanjohi-HNG-05307.py
@@ -2,7 +2,8 @@ import json
 
 user = {"name":"Wamuyu Wanjohi" , "id":"HNG-05307" , "email":"wamuyuwanjohi97@gmail.com" , "language":"python" }
 def hellouser(user):
-  print("Hello world this is " + user["name"] + " with HNGi7 ID " + user["id"] + "and email " + user["email"]+"using " + user["language"] + " for stage 2 task ." , flush=True )
-  return user
+  print("Hello world this is " + user["name"] + " with HNGi7 ID " + user["id"] + "using " + user["language"] + " for stage 2 task ." , flush=True )
+  finaluser =  json.dumps(user)
+  return finaluser
 
 hellouser(user)

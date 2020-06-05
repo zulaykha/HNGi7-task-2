7163dd6f17ee7ff42438ca755d4d99b719ff8c22
emmatessy <oluwaseun4god@gmail.com>
2020-06-05 18:12:31 +0100

    SennyJerry-HNG-06356 script updated

--- scripts/SennyJerry-HNG-06356.js
+++ scripts/SennyJerry-HNG-06356.js
@@ -1,7 +1,7 @@
-let Name = "Ajileye Oluwaseun Emmanuel";
-let ID = 'HNG-06356';
-let  email = 'oluwaseun4god@gmail.com';
-let  Language = 'Javascript';
+const Name = "Ajileye Oluwaseun Emmanuel";
+const HNGID = "HNG-06356";
+const email = "oluwaseun4god@gmail.com";
+const Language = "Javascript";
 
-let result = `Hello World, this is ${Name} with HNGi7 ID ${ID} and email ${email}  using ${Language} for stage 2 task`
+const result = `Hello World, this is ${Name} with HNGi7 ID ${HNGID} and email ${email} using ${Language} for stage 2 task`
 console.log(result);

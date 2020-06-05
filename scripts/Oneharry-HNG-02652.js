e3c6e9dbfd73fd53a9d7fb5c1144ef6afdedc205
Oneharry <harryofficialchidi@yahoo.com>
2020-06-05 19:45:38 +0100

    update HNG-02652 script

--- scripts/Oneharry-HNG-02652.js
+++ scripts/Oneharry-HNG-02652.js
@@ -2,5 +2,5 @@ let myFullName = "Ezugwu Harrison";
 let myHngId = "HNG-02652";
 let lang = "Javascript";
 let myEmail = "harryofficialchidi@yahoo.com";
-console.log(`Hello World, this is ${myFullName} with HNGi7 ID ${myHngId}
-and email ${myEmail} using ${lang} for stage 2 task`);
+console.log("Hello World, this is "+myFullName+" with HNGi7 ID " +myHngId+
+" and email "+myEmail+" using "+lang+" for stage 2 task");

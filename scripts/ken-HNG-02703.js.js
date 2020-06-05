3a9f79da26b1f17f6cf4d2b0b47bbf22310871e3
ken <iyanyakenneth@gmail.com>
2020-06-05 19:02:25 +0100

    ken-HNG-02703 script

---
+++ scripts/ken-HNG-02703.js.js
@@ -0,0 +1,7 @@
+const name = "Iyanya Kenneth";
+const HNGi7_ID = "HNG-02703";
+const language = "JavaScript";
+const email  = "iyanyakenneth@gmail.com"
+
+const string = "Hello World, this is $(name) with HNGi7 ID $(HNGi7_ID) and email $(email) using $(language) for stage 2 task";
+console.log(string);

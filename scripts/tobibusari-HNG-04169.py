700a573b58f945f6e3d5ad3c8b6afe0dbf70d295
Maryam Busari <merylbom@gmail.com>
2020-06-05 06:32:49 +0100

    'tobibusari-HNG-04169'

---
+++ scripts/tobibusari-HNG-04169.py
@@ -0,0 +1,9 @@
+first_name = 'Maryam'
+last_name = 'Busari'
+ID = 'HNG-04169'
+email = 'merylbom@gmail.com'
+language = 'python'
+
+script = f'Hello World, this is {first_name} {last_name} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task'
+
+print(script)

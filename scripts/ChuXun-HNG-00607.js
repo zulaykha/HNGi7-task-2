bfce66913d51b31fd25eb6656e7bc0efc959e982
zulaykha <62285385+zulaykha@users.noreply.github.com>
2020-06-06 02:28:50 +0100

    Update ChuXun-HNG-00607.js

--- scripts/ChuXun-HNG-00607.js
+++ scripts/ChuXun-HNG-00607.js
@@ -1,5 +1,1 @@
-const name ="zulaykha sulaiman";
-const id = "HNG-00607";
-const email = "suliat7@gmail.com";
-const language = "javascript";
-Console.log("Hello World, this is "+ name + "with HNGi7 ID:"+ id + and "email:"  + email + "using" + language + " for stage 2 task");
+console.log("Hello World, this is Zulaykha suliaman with HNGi7 ID HNG-00607 and email suliat7@gmail.com using javascript for stage 2 task");
